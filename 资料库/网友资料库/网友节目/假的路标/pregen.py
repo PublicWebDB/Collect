3e45a65be304a9293ba3d66c1825f5e21a74b4b9
2439905184 <2439905184@qq.com>
2023-09-03 14:33:02 +0800

    补充文件

---
+++ 资料库/网友资料库/网友节目/假的路标/pregen.py
@@ -0,0 +1,19 @@
+import os
+import dominate
+from dominate.tags import *
+from dominate.util import *
+doc = dominate.document(title='网友节目')
+with doc.head:
+    meta(charset="utf-8")
+with doc:
+    with h2("假的路标"):
+        pass
+    filenames=os.listdir()
+    for filename in filenames:
+        if not filename.endswith("py") and not filename.endswith("html"):
+            with p(filename):
+                pass
+            with audio(src=f'/收录网站/资料库/网友资料库/网友节目/假的路标/{filename}',controls="true"):
+                pass
+with open("index.html","w",encoding="utf-8") as f:
+    f.write(doc.render())
